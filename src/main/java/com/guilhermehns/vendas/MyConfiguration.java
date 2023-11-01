29213ecdd211105af9d63f7decd1260966f6d143
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2023-10-31 21:05:22 -0300

    Criado ambientes e profiles

--- src/main/java/com/guilhermehns/vendas/MyConfiguration.java
+++ src/main/java/com/guilhermehns/vendas/MyConfiguration.java
@@ -1,10 +1,19 @@
 package com.guilhermehns.vendas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class MyConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 
 }
