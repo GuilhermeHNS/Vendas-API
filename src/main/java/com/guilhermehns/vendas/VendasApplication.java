beeacc6bdfa790f3185241ee9d0d22e509d8e11f
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2023-11-01 22:10:56 -0300

    Criado configuração de banco de dados
    Criado entity de Cliente, Pedido, Produto, ItemPedido.
    
    Criado repositorio de Cliente com metodos de cadastrar e buscar todos os clientes.

--- src/main/java/com/guilhermehns/vendas/VendasApplication.java
+++ src/main/java/com/guilhermehns/vendas/VendasApplication.java
@@ -1,23 +1,32 @@
 package com.guilhermehns.vendas;
 
+import com.guilhermehns.vendas.domain.entity.Cliente;
+import com.guilhermehns.vendas.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
-	@Value("${application.name}")
-	private String applicationName;
+	@Bean
+	public CommandLineRunner init( @Autowired Clientes clientes){
+		return args -> {
+			clientes.salvar(new Cliente("Guilherme"));
+			clientes.salvar(new Cliente("Raphael"));
 
-	@GetMapping(value = "/hello")
-	public String helloWorld(){
-		return applicationName;
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
 	}
 
 	public static void main(String[] args) {
