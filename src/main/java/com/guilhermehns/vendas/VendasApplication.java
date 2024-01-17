0f5e3de826cfa7c6e539dab1c516b15c410c27e8
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-01-16 22:45:42 -0300

    Removendo imports não utilizados Removendo geração de senha do spring security

--- src/main/java/com/guilhermehns/vendas/VendasApplication.java
+++ src/main/java/com/guilhermehns/vendas/VendasApplication.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @RestController
 public class VendasApplication extends SpringBootServletInitializer {
 
